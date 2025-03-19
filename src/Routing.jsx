6f943d4fb465e1128e532afff06640208443b8ee
Shivam-kr-patel <shivamkrpatel915@gmail.com>
2025-03-19 12:01:04 +0530

    Ecomm-using-react

---
+++ src/Routing.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import App from './App'
+import Header from './features/Header'
+import Home from './features/Home'
+import About from './features/About'
+import Shop from './features/Shop'
+import Register from './features/Register'
+import Login from './features/Login'
+import PageNotFound from './features/PageNotFound'
+import Cart from './features/Cart'
+import ProductDetails from './features/ProductDetails'
+
+const Routing = () => {
+  return (
+   <Routes>
+        <Route path='/' element={<App/>}>
+            <Route element={<Header/>}>
+                <Route index element={<Home/>}/>
+                <Route path='about' element={<About/>}/>
+                <Route path='shop' element={<Shop/>}/>
+                <Route path='register' element={<Register/>}/>
+                <Route path='cart' element={<Cart/>}/>
+                <Route path='product/details' element={<ProductDetails/>}/>
+
+            </Route>
+            <Route path='login' element={<Login/>}/>
+        </Route>
+        <Route path='*' element={<PageNotFound/>}/>
+
+   </Routes>
+  )
+}
+
+export default Routing
