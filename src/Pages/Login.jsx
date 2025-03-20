1a5a00f22d114ac0ab16abe0b0ef70bf89007b1d
Shivam-kr-patel <shivamkrpatel915@gmail.com>
2025-03-20 11:39:48 +0530

    code-optimise

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,17 +3,9 @@ import React from 'react'
 const Login = () => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img
-        alt="Your Company"
-        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-        className="mx-auto h-10 w-auto"
-      />
-      <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-        Sign in to your account
-      </h2>
-    </div>
-
+    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+        Login to your account
+    </h2>
     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
       <form action="#" method="POST" className="space-y-6">
         {/* from here email start */}
@@ -50,7 +42,25 @@ const Login = () => {
             />
           </div>
         </div>
-
+          {/* confirm password */}
+        <div>
+          <div className="flex items-center justify-between">
+            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+              Confirm Password
+            </label>
+          </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              autoComplete="current-password"
+              className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+            />
+          </div>
+        </div>
+        
         <div>
           <button
             type="submit"
