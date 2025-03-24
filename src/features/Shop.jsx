5c8c741964841d5a5825afcc03bbe7c71f4896b3
Shivam-kr-patel <shivamkrpatel915@gmail.com>
2025-03-24 21:55:59 +0530

    code-opt

--- src/features/Shop.jsx
+++ src/features/Shop.jsx
@@ -8,7 +8,7 @@ const [products,setProducts] = useState([])
 
 useEffect(()=>{
   getData("https://dummyjson.com/products").then((result)=>{
-    console.log(result.products)
+
    setProducts(result.products)
   })
 },[])
@@ -19,9 +19,6 @@ useEffect(()=>{
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-4 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">Products</h2>
-        {/* {products.length==0 && <>
-            <button class=" border-blue-700 border-4 border-t-transparent animate-spin rounded-full h-10 w-10"></button><br/>
-                <h1>No product found</h1></>} */}
 
 {products.length==0 && <Loader/>}
 
